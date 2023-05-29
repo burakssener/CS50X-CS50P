3f0d0d936504aa2347008383b931a87391c106ca
burakssener <134919601+burakssener@users.noreply.github.com>
2023-05-29 18:17:21 +0000

    Mon, May 29, 2023, 9:17 PM +03:00

--- population.c
+++ population.c
@@ -4,12 +4,17 @@
 int main(void)
 {
     //prompting user for a starting point and goal
-    int start = get_int("How many lamas do you have? ");
+    int lama = get_int("How many lamas do you have? ");
     int goal = get_int("How many Lamas do you want? ");
-
+    int change = lama/12;
+    int year;
     //with loop find how many years user need
-    for(i = start; i<goal; i=i+i/3)
-    
+
+    for(year = 0; lama<goal; lama=lama+change)
+    {
+        year = year+1;
+    }
+
     //give result
-    printf("You need %i years to reach %i Lamas/n",i ,goal);
+    printf("You need %i years to reach %i Lamas/n",year ,lama);
 }
