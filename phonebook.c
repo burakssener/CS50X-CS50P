ae252a31931c0630e0269d1c7fc4214ac2350f49
burakssener <134919601+burakssener@users.noreply.github.com>
2023-05-29 08:32:38 +0000

    Mon, May 29, 2023, 8:32 AM +00:00

--- phonebook.c
+++ phonebook.c
@@ -6,7 +6,7 @@ int main(void)
 {
     string name = get_string("Name of the person: ");
     int age = get_int("Age of the person: ");
-    long phone = get_int("Phone number: ");
+    long phone = get_long("Phone number: ");
 
 
     printf("name= %s, age= %i, phone number= %li", name, age, phone);
