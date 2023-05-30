37aa15c9d19d0485ec284ff8321a04748b526388
burakssener <134919601+burakssener@users.noreply.github.com>
2023-05-30 15:31:38 +0000

    Tue, May 30, 2023, 6:31 PM +03:00

--- alpha.c
+++ alpha.c
@@ -6,7 +6,7 @@ int main(void)
     //get word from user
     string word = get_string("Write the word that you want to check: ");
     //determine if the word is alphebatical or not
-    int length = strlen(word);
+    int length = str_len(word);
     for(i=0; i<length; i++)
     {
         if (word[i] < word[i + 1]);
