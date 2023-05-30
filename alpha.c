2e4c071fe6db7296b2b007063642013aca07bd44
burakssener <134919601+burakssener@users.noreply.github.com>
2023-05-30 15:35:15 +0000

    Tue, May 30, 2023, 6:35 PM +03:00

--- alpha.c
+++ alpha.c
@@ -8,10 +8,11 @@ int main(void)
     string word = get_string("Write the word that you want to check: ");
     //determine if the word is alphebatical or not
     int length = strlen(word);
+    int i;
     for(i=0; i<length; i++)
     {
-        if (word[i] < word[i + 1]);
-        return;
+        if (word[i] < word[i + 1])
+        return 0;
         else;
         printf("The word is not alphebitical");
         break;
