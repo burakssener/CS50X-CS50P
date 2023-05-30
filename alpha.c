b9f7edcbe19187708aef17f6a6448d337e72f444
burakssener <134919601+burakssener@users.noreply.github.com>
2023-05-30 15:34:06 +0000

    Tue, May 30, 2023, 6:34 PM +03:00

--- alpha.c
+++ alpha.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     //get word from user
     string word = get_string("Write the word that you want to check: ");
     //determine if the word is alphebatical or not
-    int length = str_len(word);
+    int length = strlen(word);
     for(i=0; i<length; i++)
     {
         if (word[i] < word[i + 1]);
