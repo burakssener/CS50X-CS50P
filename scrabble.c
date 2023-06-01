d36fb6c0530641b5eaa5db2bea9d01ab271596b1
burakssener <134919601+burakssener@users.noreply.github.com>
2023-06-01 16:33:34 +0000

    Thu, Jun 1, 2023, 7:33 PM +03:00

--- scrabble.c
+++ scrabble.c
@@ -5,7 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char alpha[] = {"a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
 
 int compute_score(string word);
 
@@ -28,11 +28,9 @@ int compute_score(string word)
     total = 0;
     for(i=0; i < strlen(word)); i++;
     {
-        if isupper(word[i])
-        {
-            word[i] = string(int(word[i])+32)
+        printf("%c", toupper(word[i]))
+    }
 
-        }
 
 
 
