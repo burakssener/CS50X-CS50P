f5695b1e18a28265e5874a7d61d5dee1971e786f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-09-22 08:50:36 +0000

    Fri, Sep 22, 2023, 11:50 AM +03:00

--- half/half.c
+++ half/half.c
@@ -18,5 +18,6 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    return 0.0;
+    float x = bill_amount + tax_percent + tip_percent
+    return x;
 }
