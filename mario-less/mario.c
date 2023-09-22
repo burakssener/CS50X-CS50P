9087695c393c276f98b8e98647c8366f23299db9
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-09-22 11:04:14 +0000

    Fri, Sep 22, 2023, 2:04 PM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,14 +4,14 @@
 int main(void)
 {
     //prompt for input
+    int x;
 
     do
     {
-          int x = get_int("Please Provide me a number");
-          printf("Error: The number should be in the range of 1 to 8! \n")
+          x = get_int("Please Provide me a number between 1 and 8: ");
 
     }
-    while(8<=x && x<=1)
+    while(x>=8 && x<=1);
     //controls input is between 1 and 8
 
 }
