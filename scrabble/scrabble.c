1ed3674c22d0401dce8939aedb3c1cea41a6eacb
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-09-27 06:28:15 +0000

    Wed, Sep 27, 2023, 9:28 AM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -39,7 +39,7 @@ int main(void)
 int compute_score(string word)
 {
     int len = strlen(word);
-    char lowered[len+1] = word;
+    string lowered[len+1] = word;
     i = 0;
 
     while(lowered[i]!='\0')
