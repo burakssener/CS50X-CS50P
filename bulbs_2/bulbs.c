4d0e26b4d2276fcdc0bd149133053282a9d0fdc0
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-09-28 07:43:46 +0000

    Thu, Sep 28, 2023, 10:43 AM +03:00

---
+++ bulbs_2/bulbs.c
@@ -0,0 +1,76 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+void binary_get(string word);
+
+int main(void)
+{
+
+    string word = get_string("Word: ");
+
+
+}
+    int i;
+    for(i=0; word[i] != '\0' ; i++)
+    {
+        int digit = word[i];
+        while(digit != 0)
+        {
+
+            if(digit % 2 != 0)
+            {
+                print_bulb(0)
+
+                digit = digit / 2;
+
+            }
+            else if( digit % 2 != 1)
+            {
+
+                print_bulb(1)
+
+                digit = digit / 2;
+            }
+        }
+
+
+    }
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
+
+/*for(i=0; word[i] != '\0' ; i++)
+    {
+
+        int digit = word[i];
+        while(digit != 0)
+        {
+
+            if(digit % 2 != 0)
+            {
+                strcat(binary, "0");
+                digit = digit / 2;
+
+            }
+            else if( digit % 2 != 1)
+            {
+
+                strcat(binary, "1");
+                digit = digit / 2;
+            }
+            return 0; /*
