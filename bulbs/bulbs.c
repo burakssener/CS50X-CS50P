822a7c4790fa65f3966c7cf5b0dd9a8debbf3028
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-09-29 06:48:17 +0000

    Fri, Sep 29, 2023, 9:48 AM +03:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -9,7 +9,14 @@ void print_bulb(int bit);
 int main(void)
 {
  // for loop to get strings characters
- for(i=0; i<strlen(word))
+ for(i=0; i<strlen(word); i++)
+ {
+    
+
+
+
+
+ }
  // for loop inside for loop to get binary code of characters
  // printing these binary codes into display screen
 }
