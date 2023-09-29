136584d370ff49b7998d1f3b747702cb1b3e4452
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-09-29 07:11:20 +0000

    Fri, Sep 29, 2023, 10:11 AM +03:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -34,7 +34,7 @@ int main(void)
                 digit = digit / 2;
             }
         }
-        for(j = 7; j >=0; j--)
+        for(j = 7; j >= 0; j--)
         {
             print_bulb(binary[j]);
 
