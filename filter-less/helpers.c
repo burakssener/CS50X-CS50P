9cf824b193e4c383e215f2ac874102c6b864a8ec
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-13 11:25:09 +0000

    Fri, Oct 13, 2023, 2:25 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -6,18 +6,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 
     for(a = 0; a < height; a = a + 1)
     {
-        average = (image[a][a].rgbtRed + image[a][a].rgbtGreen + image[a][a].rgbtBlue) / 3;
+
         for (b = 0; b < width; b = b + 1)
         {
-
+            average = (image[a][b].rgbtRed + image[a][b].rgbtGreen + image[a][b].rgbtBlue) / 3;
             image[a][b].rgbtRed = average;
             image[a][b].rgbtGreen = average;
             image[a][b].rgbtBlue = average;
-
-
-
-
-
         }
 
     }
