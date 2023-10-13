a55960b591e62cdc93cf4017f7ab4504aed001a7
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-13 11:21:09 +0000

    Fri, Oct 13, 2023, 2:21 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,9 +3,11 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    image[height][width].rgbtRed + image[height][width].rgbtGreen;
-    image[height][width].rgbtGreen;
-    image[height][width].rgbtBlue;
+    average = (image[height][width].rgbtRed + image[height][width].rgbtGreen + image[height][width].rgbtBlue) / 3;
+    for(height = 0, )
+    image[height][width].rgbtRed = average;
+    image[height][width].rgbtGreen = average;
+    image[height][width].rgbtBlue = average;
     return;
 }
 
