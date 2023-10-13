7058ba8a111b03526d4cc63edbb0cce9cc5ddf55
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-13 11:59:40 +0000

    Fri, Oct 13, 2023, 2:59 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -65,6 +65,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    int average, a, b;
+    for(a = 0; a < height; a = a + 1)
+    {
+
+        for (b = 0; b < width; b = b + 1)
+        {
+            average = round((image[a][b].rgbtRed + image[a][b].rgbtGreen + image[a][b].rgbtBlue) / 3.0);
+            image[a][b].rgbtRed = average;
+            image[a][b].rgbtGreen = average;
+            image[a][b].rgbtBlue = average;
+        }
+
+    }
+
+    return;
     return;
 }
 
