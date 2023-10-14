e820dd939a4b2bbfa0a82587c5e0ff0e885d6822
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-14 17:39:00 +0000

    Sat, Oct 14, 2023, 8:39 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,6 +14,11 @@ FILE *file = fopen(argv[1], "r");
 uint8_t buffer[BLOCK_SIZE];
 if (buffer[0] == "0xff" && buffer[1] == "0xd8" && buffer[2] == "0xff" &&  ((buffer[3] & "0xf0") == "0xe0"))
 {
+    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+{
+
+
+}
 
 
 }
@@ -27,10 +32,6 @@ sprintf(filename, "%03i.jpg", 2);
 
 FILE *img = fopen(filename, "w");
 
-while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
-{
 
-
-}
 
 fwrite()
