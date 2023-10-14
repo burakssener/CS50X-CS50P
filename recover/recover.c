36d39e5a678d69a46a1d4b51bf7e088851bfc93a
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-14 17:20:12 +0000

    Sat, Oct 14, 2023, 8:20 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#define BLOCK_SIZE 512;
 int main(int argc, char *argv[])
 {
- 
+    if(argc != 2)
+    {
+        return 1;
+    }
+
+FILE *file = fopen(argv[1], "r");
+
+}
+
+uint8_t buffer[BLOCK_SIZE];
+
+if (buffer[0] == "0xff" && buffer[0] == "0xff" && buffer[0] == "0xff" &&)
+while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+{
+
+
 }
