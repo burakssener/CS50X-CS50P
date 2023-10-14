279fa95e4a21596fc3a61bc8c0b34395fdda0f64
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-14 18:15:09 +0000

    Sat, Oct 14, 2023, 9:15 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#define BLOCK_SIZE 512;
+#define BLOCK_SIZE 512
 int main(int argc, char *argv[])
 {
     if(argc != 2)
@@ -13,16 +13,19 @@ int main(int argc, char *argv[])
 FILE *raw_file = fopen(argv[1], "r");
 // look for begginning of a jpeg
 uint8_t buffer[BLOCK_SIZE];
-fread(buffer, i, BLOCK_SIZE, raw_file)
+int jpegcount = 0;
+fread(buffer, 1, BLOCK_SIZE, raw_file);
 
-if (buffer[0] == "0xff" && buffer[1] == "0xd8" && buffer[2] == "0xff" &&  ((buffer[3] & "0xf0") == "0xe0"))
+if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&  ((buffer[3] & 0xf0) == 0xe0))
 {
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-        char *filename = sprintf(filename, "%03i.jpg", jpegcount);
+        char *filename
+        sprintf(filename, "%03i.jpg", jpegcount);
         FILE *img = fopen(filename, "w");
         fwrite(buffer, i, BLOCK_SIZE, raw_file);
         jpegcount = jpegcount + 1;
+
     }
 
 
