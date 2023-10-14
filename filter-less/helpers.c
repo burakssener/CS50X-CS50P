5cfef9f58ccdf23f8546fb6286278d61e329ad78
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-14 13:52:30 +0000

    Sat, Oct 14, 2023, 4:52 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -83,10 +83,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
         else{
 
-            for (b = 0; b < width; b = b + 1)
+            for (b = 0; b != (width + 1) / 2; b = b + 1)
         {
-            if(b != (width + 1) / 2)
-            *tmp = image[a][b];
+            tmp = image[a][b];
             image[a][b] = image[height-1-a][width-1-b];
             image[height-1-a][width-1-b] = tmp;
 
