1cc017119fbfe08deabc809d6396649744958003
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-14 17:43:00 +0000

    Sat, Oct 14, 2023, 8:43 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,15 +9,16 @@ int main(int argc, char *argv[])
     }
 
 //open memory card
-FILE *file = fopen(argv[1], "r");
+FILE *raw_file = fopen(argv[1], "r");
 // look for begginning of a jpeg
 uint8_t buffer[BLOCK_SIZE];
 if (buffer[0] == "0xff" && buffer[1] == "0xd8" && buffer[2] == "0xff" &&  ((buffer[3] & "0xf0") == "0xe0"))
 {
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
+        char *filename = sprintf(filename, "%03i.jpg", 2);
         FILE *img = fopen(filename, "w");
-        sprintf(filename, "%03i.jpg", 2);
+
 
 
     }
