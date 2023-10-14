4dbd67f93ec357e2b7654059849b93c8bf2b1210
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-14 17:39:48 +0000

    Sat, Oct 14, 2023, 8:39 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -15,10 +15,10 @@ uint8_t buffer[BLOCK_SIZE];
 if (buffer[0] == "0xff" && buffer[1] == "0xd8" && buffer[2] == "0xff" &&  ((buffer[3] & "0xf0") == "0xe0"))
 {
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
-{
+    {
 
 
-}
+    }
 
 
 }
