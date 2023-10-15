85655b252802d19c77c55a9649f451715ad33990
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-15 06:59:30 +0000

    Sun, Oct 15, 2023, 9:59 AM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,21 +17,26 @@ FILE *output_file = NULL;
 uint8_t buffer[BLOCK_SIZE];
 int jpegcount = 0;
 fread(buffer, 1, BLOCK_SIZE, raw_file);
+char *filename = malloc(8);
 
-if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&  ((buffer[3] & 0xf0) == 0xe0))
-{
+
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-        char *filename = malloc(sizeof(char) * 8);
-        sprintf(filename, "%03i.jpg", jpegcount);
-        FILE *img = fopen(output_file, "w");
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&  ((buffer[3] & 0xf0) == 0xe0))
+        {
+            sprintf(filename, "%03i.jpg", jpegcount);
+            FILE *img = fopen(output_file, "w");
+            jpegcount = jpegcount + 1;
+
+        }
+
+
         fwrite(buffer, 1, BLOCK_SIZE, img);
-        jpegcount = jpegcount + 1;
 
     }
 
 
-}
+
 // open a new jpeg file
 // write 512 bytes until new jpeg is found
 
