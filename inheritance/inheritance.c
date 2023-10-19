2bda04e3b4a2554e3696bd87347eade659e7cfff
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-19 07:19:18 +0000

    Thu, Oct 19, 2023, 10:19 AM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -48,8 +48,8 @@ person *create_family(int generations)
     if (generations > 1)
     {
         // Create two new parents for current person by recursively calling create_family
-        person *parent0 = create_family(generations - 1);
-        person *parent1 = create_family(generations - 1);
+        person_1->parent0 = create_family(generations - 1);
+        person_1->parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
 
