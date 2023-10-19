2129c5587455ced3d80032f3c61a3ec7ed52d788
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-19 07:57:49 +0000

    Thu, Oct 19, 2023, 10:57 AM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -57,7 +57,7 @@ person *create_family(int generations)
         (*person_1).alleles[1] = person_1->parents[1]->alleles[rand() % 2];
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+    }
 
     // If there are no generations left to create
     else
