6608af3f58e82b5fb6eb001fdb34dc7bd54d539a
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-23 13:12:09 +0000

    Mon, Oct 23, 2023, 4:12 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -97,7 +97,9 @@ bool unload(void)
     node *n = malloc(sizeof(node));
     node *tmp = malloc(sizeof(node));
     tmp = table[0]->next;
+    while(n !=)
     n= tmp->next;
+    free(tmp)
     // TODO
     return false;
 }
