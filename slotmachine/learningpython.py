60720b62e132ce02045342d9a2aa9d77917d7e46
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-26 05:08:14 +0000

    Thu, Oct 26, 2023, 8:08 AM +03:00

--- slotmachine/learningpython.py
+++ slotmachine/learningpython.py
@@ -1,3 +1,7 @@
 def deposit():
     while True:
-        amount = input("What would you like to")
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            sadasda
+        else
+            
