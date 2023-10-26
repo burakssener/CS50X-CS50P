e82b9e98d3dc9513c7e8dc2f4ec1d942e09326fe
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-26 05:02:13 +0000

    Thu, Oct 26, 2023, 8:02 AM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,14 +16,9 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open(filename) as file:
-      teams = csv.DictReader(file)
-      print(teams)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    for tournaments in range(N):
-        simulate_tournament(teams)
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -54,8 +49,6 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    for win in range(4):
-        simulate_round(teams)
     # TODO
 
 
