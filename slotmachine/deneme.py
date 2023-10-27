013c88fb3ca3f857f507c70ba86a3720748029bc
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-27 19:10:47 +0000

    Fri, Oct 27, 2023, 10:10 PM +03:00

---
+++ slotmachine/deneme.py
@@ -0,0 +1,8 @@
+nums = [1 , 2, 3, 4, 5, 3]
+nums2 = []
+        for num in nums:
+            nums2.append(num)
+            for num2 in nums2:
+                if num2 == num:
+                    return false
+        return True
