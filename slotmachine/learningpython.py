0cca3a2359097e47f155f78baa4d7544f501d6f5
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-27 08:29:54 +0000

    Fri, Oct 27, 2023, 11:29 AM +03:00

--- slotmachine/learningpython.py
+++ slotmachine/learningpython.py
@@ -18,9 +18,9 @@ def print_slot(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end =" | ")
             else:
-                print (column[row])
+                print (column[row], end="")
         print(" ")
 
 
