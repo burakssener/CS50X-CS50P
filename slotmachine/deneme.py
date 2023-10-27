b8476a7f9cc7d970e192d6ce6b318c1e9c2e41a2
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-27 19:12:47 +0000

    Fri, Oct 27, 2023, 10:12 PM +03:00

--- slotmachine/deneme.py
+++ slotmachine/deneme.py
@@ -1,8 +1,11 @@
-nums = [1 , 2, 3, 4, 5, 3]
-nums2 = []
-        for num in nums:
-            nums2.append(num)
-            for num2 in nums2:
-                if num2 == num:
-                    return false
-        return True
+
+def get_duplicate():
+    nums = [1 , 2, 3, 4, 5, 3]
+    nums2 = []
+    for num in nums:
+        nums2.append(num)
+        for num2 in nums2:
+            if num2 == num:
+                return True
+            return False
+print(get_duplicate())
