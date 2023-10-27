7b412354a44f2410b6853514dc803941c79fdd75
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-27 07:48:43 +0000

    Fri, Oct 27, 2023, 10:48 AM +03:00

--- slotmachine/learningpython.py
+++ slotmachine/learningpython.py
@@ -1,7 +1,16 @@
 MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 100
-ROWS 
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
 def get_slot(rows, cols, symbols):
 
 
