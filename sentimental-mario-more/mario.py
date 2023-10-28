288d97dba3ab889a2292b318bd1e1be45949f019
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 07:13:39 +0000

    Sat, Oct 28, 2023, 10:13 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,4 +1,6 @@
-height = int(input("Height: "))
+height = 0
+while !(height>= 1 and height<= 8):
+    height = int(input("Height: "))
 for i in range(height):
     print(" " * (height-i-1), end="")
     print("#" * (i+1), end="  ")
