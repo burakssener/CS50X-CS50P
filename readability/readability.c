2cac8fa9ae0ee353f5994f861a01f56cc27d0d0f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 14:46:55 +0000

    Sat, Oct 28, 2023, 5:46 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <cytpe.h>
+#include <ctype.h>
 
 int main(void)
 {
     double index;
-    
+    char *input = get_string("Text: ");
+    printf("%c", input[0]);
+    /*for(i = 0; i!= '/0'; i++)
+    {
 
-    index = 0.0588 * L - 0.296 * S - 15.8;
+    }
+
+    index = 0.0588 * L - 0.296 * S - 15.8;*/
 }
