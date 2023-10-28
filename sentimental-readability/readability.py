39c5e8c8d7e1cb234e6effd7a57cdefc103bab9a
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 19:53:24 +0000

    Sat, Oct 28, 2023, 10:53 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,23 @@
 input = input("Text:")
-
+word , letter , sentence = 0, 0, 0
 for c in input:
     if(c.isspace()):
-        word +=1
-    elif(c.isupper())
+        word += 1
+    elif(c.isupper() or c.islower()):
+        letter += 1
+    elif(c == "!" or c == "." or c == "?"):
+        sentence += 1
+
+word += 1
+
+the_L = letter / word * 100;
+the_S = sentence / word * 100;
+index = 0.0588 * the_L - 0.296 * the_S - 15.8;
+grade = round(index)
+
+if grade < 1:
+    print("Before Grade 1")
+elif grade >16:
+    print("Grade 16+")
+else:
+    print(f"Grade {grade}")
