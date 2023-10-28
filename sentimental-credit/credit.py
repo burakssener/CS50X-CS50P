b2cb2112c734f9b3e31b6c38f300562f57c1a764
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 12:24:37 +0000

    Sat, Oct 28, 2023, 3:24 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,3 +2,7 @@
 # american express 15 digits starts with 34 or 37
 #master card 16 digits, starts with 51, 52, 53, 54, or 55
 # Visa 13 or 16 digits starts with 4
+
+input = input("Enter a Credit card number")
+
+F
