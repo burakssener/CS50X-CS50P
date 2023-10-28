f0e679fe9fd9ed2b74027ef46f1a3b50ddcd9d36
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 15:24:06 +0000

    Sat, Oct 28, 2023, 6:24 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,36 +5,43 @@
 int main(void)
 {
     double index, the_L, the_S;
-    int i, word = 0, letter = 0, sentence = 0, grade;
+    int i, words = 0, letters = 0, sentences = 0, grade;
     char *input = get_string("Text: ");
 
     for(i = 0; input[i] != '\0'; i++)
     {
         if(isblank(input[i]))
         {
-            word += 1;
+            words += 1;
         }
         if(islower(input[i]) || isupper(input[i]))
         {
-            letter += 1;
+            letters += 1;
         }
         if(input[i] == '!' || input[i] == '.' || input[i] == '?')
         {
-            sentence += 1;
+            sentences += 1;
         }
 
     }
-    word = word + 1;
-    the_L = 1.0 * letter / words * 100;
-    the_S = 1.0 * sentence / words * 100;
+    words += 1;
+    the_L = 1.0 * letters / words * 100;
+    the_S = 1.0 * sentences / words * 100;
     index = 0.0588 * the_L - 0.296 * the_S - 15.8;
     grade = round(index);
     if(grade <= 1)
     {
-        printf("Grade 1";)
+        printf("Grade 1");
     }
     else if(grade >= 16)
     {
-        printf("Grade 16")
+        printf("Grade 16");
     }
+    else
+    {
+        printf("Grade %d", grade);
+    }
+
+
+
 }
