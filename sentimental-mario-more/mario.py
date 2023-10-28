a2aafc4c353ce2cf8dfaacd1e8f87d8ba35e5a7b
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 07:10:33 +0000

    Sat, Oct 28, 2023, 10:10 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,5 +1,5 @@
 height = int(input("Height: "))
 for i in range(height):
-    print(" " * (height-i+1), end="")
+    print(" " * (height-i-1), end="")
     print("#" * (i+1), end="  ")
     print("#" * (i+1))
