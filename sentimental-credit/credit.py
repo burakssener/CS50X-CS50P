5a38c6c361c389844a0d07c9c792bb4c7da72dc1
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 12:56:51 +0000

    Sat, Oct 28, 2023, 3:56 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,30 +7,33 @@ while True:
     input = input("Enter a Credit card number")
     length = len(input)
     if input.isdigit():
-        input = int(input)
         break
 
 list = []
 for num in range(length):
     list.append(int(input[num]))
+even, odd = 0, 0
 for i in range(length):
     if i % 2 == 1:
-        term = input[i] * 2
+        term = int(input[i]) * 2
         if(term > 9):
             term = (term % 10) + 1
         even += term
     else:
-        odd += input[i]
+        odd += int(input[i])
 
 if (odd + even) % 10 != 0:
     print("INVALID")
     exit()
 
-item_num = len(input)
-print(input[0:1])
+two_digits = int(input[0:2])
 
-"""if item_num == 15 and (input[0:1] == 34 or input[0:1]== 37):
+if length == 15 and (two_digits == 34 or two_digits== 37):
     print("AMEX")
-elif item_num == 16 and """
-
+elif length == 16 and (51 <= two_digits <= 55):
+    print("MASTERCARD")
+elif length == 14 and two_digits in range(40,49):
+    print("VISA")
+else:
+    print("INVALID")
 
