b797aaa8a4a6c900f1c17d8f39a1760b4b0427b7
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 07:21:40 +0000

    Sat, Oct 28, 2023, 10:21 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,11 @@
 
 height = 0
-while not (height>= 1 and height<= 8):
+while True:
     height = input("Height: ")
-    if height isdigit():
+    if height.isdigit():
         height = int(height)
+        if (height>= 1 and height<= 8):
+            break
 for i in range(height):
     print(" " * (height-i-1), end="")
     print("#" * (i+1), end="  ")
