b28492d7050fe944d16fb1f6f3b76b29afdd728b
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-28 08:22:53 +0000

    Sat, Oct 28, 2023, 11:22 AM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,7 +3,28 @@
 
 int main(void)
 {
+    int digit[], i = 0;
     long input = get_long("Please provide a credit number");
+    while(!(input / 10 == 0))
+    {
+        digit[i] = input % 10;
+        input = input / 10;
+        i += 1;
+    }
+    digit[i] = input;
+
+    if ((i + 1) == 15)
+    {
+
+    }
+    else if ((i + 1) == 16)
+    {
+
+    }
+    else if ((i + 1) == 13 && (i + 1) == 16)
+    {
+        
+    }
 
 
 }
