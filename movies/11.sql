fe60310fbf407bffda8a6d90c11cd5f7b9cc6f2e
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 05:09:51 +0000

    Tue, Oct 31, 2023, 8:09 AM +03:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,7 +1,8 @@
 SELECT title
 FROM movies
-JOIN 
-
-SELECT title
-FROM movies
+JOIN ratings ON ratings.movie_id = movies.id
 WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman" )))
+ORDER BY rating
+LIMIT 5;
+SELECT title
+FROM movies;
