d3409043ddc1f7157dad07106f272aee0214e0f6
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 05:01:13 +0000

    Tue, Oct 31, 2023, 8:01 AM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,3 +1,3 @@
 SELECT name
 FROM people
-WHERE id = (SELECT person_id FROM directors WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating > 9.0));
+WHERE id IN (SELECT person_id FROM directors WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating > 9.0));
