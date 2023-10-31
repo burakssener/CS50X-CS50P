c887d5fcc2e6231ab6257b12a0a12fc68832a2c2
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 04:58:08 +0000

    Tue, Oct 31, 2023, 7:58 AM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,3 @@
-SELECT DISTINCT(name)
+SELECT name
 FROM people
-JOIN directors ON directors.person_id = people.id
-WHERE = (SELECT id)
-WHERE rating >= 9.0;
+WHERE id = (SELECT person_id FROM directors WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating > 9.0));
