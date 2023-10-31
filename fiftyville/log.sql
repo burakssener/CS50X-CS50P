b5f87b730898358bf7701ed7282a7c7cd908bb3f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 19:32:19 +0000

    Tue, Oct 31, 2023, 10:32 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -26,12 +26,12 @@ WHERE month = 7 AND day = 28 AND hour = 10;
 
 
 
---PEOPLE'S LICENCE PLATES THAT ENTER AND EXIT TO THE BAKERY IN THE DAY THAR DUCK IS STOLEN:
+--PEOPLE'S LICENCE PLATES THAT ENTER AND EXIT TO THE BAKERY IN THE TIME THAT DUCK IS STOLEN:
 SELECT license_plate
 FROM bakery_security_logs
-WHERE month = 7 AND day = 28 AND  activity = "entrance" AND license_plate IN (SELECT license_plate
+WHERE month = 7 AND day = 28 AND (hour = 9 OR hour = 10) AND  activity = "entrance" AND license_plate IN (SELECT license_plate
 FROM bakery_security_logs
-WHERE month = 7 AND day = 28 AND activity = "exit");
+WHERE month = 7 AND (hour = 9 OR hour = 10) AND day = 28 AND activity = "exit");
 
 -- looking phone calls TABLE
 SELECT *
@@ -46,7 +46,7 @@ WHERE license_plate IN (SELECT license_plate
 FROM bakery_security_logs
 WHERE month = 7 AND day = 28 AND (hour = 9 OR hour = 10) AND  activity = "entrance" AND license_plate IN (SELECT license_plate
 FROM bakery_security_logs
-WHERE month = 7 AND day = 28 AND (hour = 10 0R hour = 11) AND activity = "exit"));
+WHERE month = 7 AND day = 28 AND (hour = 10 OR hour = 11) AND activity = "exit"));
 
 -- Phone calls that happened on the case day from these people and phone numbers as a receiver or caller
 
