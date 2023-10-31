edda0e7b3392db692cedb25522ac0b8a4daa4df9
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 19:39:33 +0000

    Tue, Oct 31, 2023, 10:39 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -66,3 +66,9 @@ FROM bakery_security_logs
 WHERE month = 7 AND day = 28 AND (hour = 9 OR hour = 10) AND  activity = "entrance" AND license_plate IN (SELECT license_plate
 FROM bakery_security_logs
 WHERE month = 7 AND day = 28 AND (hour = 10 OR hour = 11) AND activity = "exit")));
+
+--looking for interviews
+
+SELECT *
+FROM interviews
+WHERE month = 7 AND day = 28 AND street = "Humphrey Street";
