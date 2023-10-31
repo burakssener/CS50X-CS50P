2c861f0542b4ddd2daa4d07689049bbfe8289f0f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 04:44:56 +0000

    Tue, Oct 31, 2023, 7:44 AM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,5 @@
-SELECT name
+SELECT DISTINCT(name)
 FROM people
 JOIN stars on stars.person_id = people.id
-WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004);
+WHERE movie_id IN (SELECT id FROM movies WHERE year = 2004)
+ORDER BY birth;
