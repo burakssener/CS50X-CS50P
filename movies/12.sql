0d418419c0b297c73df9f333d0cd8654abf2e165
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 05:18:43 +0000

    Tue, Oct 31, 2023, 8:18 AM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,3 @@
 SELECT title
 FROM movies
-WHERE id = (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Jennifer Lawrence" or name = "Bradley Cooper"))
+WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Jennifer Lawrence" ));
