4fdeab299bf1b106c2d5f34a08eb99a465f29d8f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-10-31 05:07:50 +0000

    Tue, Oct 31, 2023, 8:07 AM +03:00

---
+++ movies/11.sql
@@ -0,0 +1,7 @@
+SELECT title
+FROM movies
+JOIN 
+
+SELECT title
+FROM movies
+WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman" )))
