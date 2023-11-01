675cf3ca8b1544e1c4d8b4815e68a07044508ce3
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-01 17:19:42 +0000

    Wed, Nov 1, 2023, 8:19 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -143,7 +143,14 @@ LIMIT 10;
 
 
 SELECT account_number
-FROM 
+FROM bank_accounts
+WHERE person_id IN (SELECT person_id
+FROM people
+WHERE license_plate IN (SELECT license_plate
+FROM bakery_security_logs
+WHERE month = 7 AND day = 28 AND (hour = 9 OR hour = 10) AND  activity = "entrance" AND license_plate IN (SELECT license_plate
+FROM bakery_security_logs
+WHERE month = 7 AND day = 28 AND (hour = 10 OR hour = 11) AND activity = "exit")));
 
 
 
