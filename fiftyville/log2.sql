e82d81149ab032778a08cbcef09734d311806381
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-02 09:17:01 +0000

    Thu, Nov 2, 2023, 12:17 PM +03:00

--- fiftyville/log2.sql
+++ fiftyville/log2.sql
@@ -1,3 +1,12 @@
 SELECT description
 FROM crime_scene_reports
 WHERE month = 7 AND day = 28 AND street = "Humphrey Street";
+--| Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
+
+SELECT *
+FROM bakery_security_logs
+WHERE month = 7 AND day = 28 AND (hour = 9 OR hour = 10) AND  activity = "entrance" AND
+
+license_plate IN (SELECT license_plate
+FROM bakery_security_logs
+WHERE month = 7 AND (hour = 10 OR hour = 11) AND day = 28 AND activity = "exit");
