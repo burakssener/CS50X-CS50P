1486261be27ed077a6acdcb4b13c2877a74616ee
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-05 14:30:49 +0000

    Sun, Nov 5, 2023, 5:30 PM +03:00

--- froshims/app.py
+++ froshims/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-registrants = {}
+REGISTRANTS = {}
 
 @app.route("/")
 def index():
@@ -12,7 +12,7 @@ def index():
 def register():
     name = request.form.get("name")
     sport = request.form.get("sport")
-    registrants[name] = sport
+    REGISTRANTS[name] = sport
     return render_template("success.html")
 
 @app.route("/registrants")
