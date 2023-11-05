6ab290205d15f8bdf6193f834279d1341aa82c89
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-05 14:24:47 +0000

    Sun, Nov 5, 2023, 5:24 PM +03:00

--- froshims/app.py
+++ froshims/app.py
@@ -10,4 +10,11 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    registrants[name] = sport
     return render_template("success.html")
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants= registrants)
