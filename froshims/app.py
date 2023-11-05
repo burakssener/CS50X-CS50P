686464189776570914441e7247824516ce496953
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-05 15:23:33 +0000

    Sun, Nov 5, 2023, 6:23 PM +03:00

--- froshims/app.py
+++ froshims/app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 REGISTRANTS = {}
 SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
+
+
 @app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
+
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -18,6 +21,7 @@ def register():
         return render_template("failure.html")
     REGISTRANTS[name] = sport
     return render_template("success.html")
+
 
 @app.route("/registrants")
 def registrants():
