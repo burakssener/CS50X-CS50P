fd01ea5c2ffbd2cea0d24d0aacf24b61bd55c301
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-05 14:57:05 +0000

    Sun, Nov 5, 2023, 5:57 PM +03:00

--- froshims/app.py
+++ froshims/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 REGISTRANTS = {}
-
+SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
 @app.route("/")
 def index():
     return render_template("index.html")
