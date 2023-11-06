907e27ed63f9c32526f958a1eb1a1022c0d8ada2
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-06 12:38:41 +0000

    Mon, Nov 6, 2023, 3:38 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -26,7 +26,7 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        db.execute(INSERT INTO birthdays )
+        db.execute(INSERT INTO birthdays (name, month, day))
 
         return redirect("/")
 
