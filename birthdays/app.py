109da238c995413829e959930fc1cbcdf8534007
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-06 18:33:27 +0000

    Mon, Nov 6, 2023, 9:33 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -11,7 +11,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
-day_num = list(range(1,31))
 
 @app.after_request
 def after_request(response):
