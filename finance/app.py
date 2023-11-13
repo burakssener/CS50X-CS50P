ce3b818c6740ce27a922b565ebd328e776a244d6
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-13 17:10:45 +0000

    Mon, Nov 13, 2023, 8:10 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -125,7 +125,7 @@ def register():
                 crpassword = request.form.get("cr_password")
                 users = db.execute("SELECT username FROM users WHERE username= ? ", username)
                 if  password != crpassword:
-                    not username in users.values()
+                    #not username in users.values()
                     return apology("must provide username", 403)
                 else:
                     db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, password)
