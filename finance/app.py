922a2a1492b53c10f2f71df574d72896ea0d1088
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-14 17:53:27 +0000

    Tue, Nov 14, 2023, 8:53 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -49,13 +49,14 @@ def buy():
         if not symbol:
             return apology("must provide username", 403)
         else:
-            stock_name = request.form.get("stock_name")
+            stock_name = symbol["symbol"]
             if not request.form.get("stock_num"):
                 return apology("must provide password", 403)
             else:
                 stock_num = int(request.form.get("stock_num"))
                 user_data = db.execute("SELECT id, cash FROM users WHERE id = ?", session['user_id'])[0]
-                if (user_data["cash"] >= symbol["price"] * stock_num ):
+                if (user_data["cash"] >= symbol["price"] * stock_num):
+                    if db.execute("SELECT * FROM users_balance WHERE stock_name = ? AND  ")
                     db.execute("UPDATE users SET cash = ? WHERE id = ?", user_data["cash"] - symbol["price"] * stock_num, session['user_id'] )
                     db.execute("INSERT INTO users_balance (stock_num, stock_name, user_id) VALUES (?, ?, ?)", stock_num, stock_name, session['user_id'] )
                     user_data = db.execute("SELECT users.id, users.cash, users_balance.stock_num, users_balance.stock_name FROM users JOIN users_balance ON users_balance.user_id = users.id WHERE id = ?", session['user_id'])[0]
