46ba497cf794ce50deec8907b6c610dd5669126b
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-14 17:20:36 +0000

    Tue, Nov 14, 2023, 8:20 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -54,7 +54,7 @@ def buy():
                 return apology("must provide password", 403)
             else:
                 stock_num = request.form.get("stock_num")
-                user_data = db.execute("SELECT * FROM users WHERE id = ?", session['user_id'])
+                user_data = db.execute("SELECT * FROM users WHERE id = ?", session['user_id'])[0]
                 if (user_data["cash"] >= symbol["price"] * stock_num ):
                     db.execute("UPDATE users SET cash = ? WHERE id = ?", (user_data["cash"] - symbol["price"] * stock_num) session['user_id'] )
                     db.execute("INSERT INTO users_balance (stock_num, stock_name, user_id) VALUES (?, ?, ?)", stock_num, stock_name, session['user_id'] )
