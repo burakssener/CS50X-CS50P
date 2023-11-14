dd5782f246e20370950666b4573717de4b292678
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-14 17:49:27 +0000

    Tue, Nov 14, 2023, 8:49 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -54,11 +54,11 @@ def buy():
                 return apology("must provide password", 403)
             else:
                 stock_num = int(request.form.get("stock_num"))
-                user_data = db.execute("SELECT users.id, users.cash, users_balance.stock_num, users_balance.stock_name FROM users JOIN users_balance ON users_balance.user_id = users.id WHERE id = ?", session['user_id'])[0]
+                user_data = db.execute("SELECT id, cash FROM users WHERE id = ?", session['user_id'])[0]
                 if (user_data["cash"] >= symbol["price"] * stock_num ):
                     db.execute("UPDATE users SET cash = ? WHERE id = ?", user_data["cash"] - symbol["price"] * stock_num, session['user_id'] )
                     db.execute("INSERT INTO users_balance (stock_num, stock_name, user_id) VALUES (?, ?, ?)", stock_num, stock_name, session['user_id'] )
-                    user_data = db.execute("SELECT * FROM users JOIN users_balance ON users_balance.user_id = users.id WHERE users.id = ?", session['user_id'])
+                    user_data = db.execute("SELECT users.id, users.cash, users_balance.stock_num, users_balance.stock_name FROM users JOIN users_balance ON users_balance.user_id = users.id WHERE id = ?", session['user_id'])[0]
                     return render_template("basket.html", user_data=user_data )
 
 
