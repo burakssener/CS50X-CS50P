d23a3564f655b34993796c29278c52c4bf70ba98
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-16 11:43:09 +0000

    Thu, Nov 16, 2023, 2:43 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -217,7 +217,6 @@ def sell():
             stock_name = symbol["symbol"]
             stock_num = int(request.form.get("stock_num"))
             user_data = db.execute("SELECT id, cash FROM users WHERE id = ?", session['user_id'])[0]
-            db.execute("UPDATE users SET cash = ? WHERE id = ?", user_data["cash"] + symbol["price"] * stock_num, session['user_id'] )
             for stock in stock_data:
                 if stock_name == stock["stock_name"] and real_stock_num < stock_num:
                     return apology("You don't have enough stock")
@@ -225,6 +224,7 @@ def sell():
                     db.execute("UPDATE users_balance SET stock_num = ? WHERE user_id = ? AND stock_name = ?", real_stock_num - stock_num, session['user_id'], stock_name )
                 elif stock_name == stock["stock_name"] and real_stock_num == stock_num:
                     db.execute("DELETE FROM users_balance WHERE user_id = ? AND stock_name = ?", session['user_id'], stock_name)
+            db.execute("UPDATE users SET cash = ? WHERE id = ?", user_data["cash"] + symbol["price"] * stock_num, session['user_id'] )
             user_cash = db.execute("SELECT cash FROM users WHERE id = ?", session['user_id'])
             user_data = db.execute("SELECT stock_num AS Shares, stock_name AS Name FROM users_balance WHERE user_id = ?", session['user_id'])
             total_money = 0
