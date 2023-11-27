74f3f4227f7948d7d7942e2f54b67dc207499233
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-27 11:37:51 +0000

    Mon, Nov 27, 2023, 2:37 PM +03:00

--- denemee.c
+++ denemee.c
@@ -5,6 +5,14 @@ int main(void)
 
     for (int i = 0; i<10; i++)
     {
-        table[0][i] = 
+        table[0][i] = i;
+    }
+    for (int i = 0; i<2; i++)
+    {
+        for (int a = 0; a<5; a++)
+    {
+        printf("%d", table[i][a] );
+    }
+
     }
 }
