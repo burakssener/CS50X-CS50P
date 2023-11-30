7ed12f373e6e50144f9c28c252cfbe3adabb8d3d
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-30 11:11:32 +0000

    Thu, Nov 30, 2023, 2:11 PM +03:00

---
+++ deep/deep.py
@@ -0,0 +1,5 @@
+answer(input("What is the Answer to the Great Question of Life, the Universe, and Everything?"))
+if answer isdigit():
+    int()
+if answer == "forty-two" or answer == "forty-two":
+
