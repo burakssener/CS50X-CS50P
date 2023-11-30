ef93bdda27b3aadd24269a2ff21263957334f01a
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-30 11:25:00 +0000

    Thu, Nov 30, 2023, 2:25 PM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,11 +1,12 @@
-answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?").strip
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
 if answer.isdigit():
     if int(answer) == 42:
         print("Yes")
     else:
         print("No")
 else:
-    if answer == "forty-two" or answer.lower() == "forty two":
+    answer = answer.strip()
+    if answer== "forty-two" or answer.lower() == "forty two":
         print("Yes")
     else:
         print("No")
