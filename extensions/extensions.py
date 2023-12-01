e7e55101536c28810bfd7ce26c4fed7c435bbe35
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 09:01:53 +0000

    Fri, Dec 1, 2023, 12:01 PM +03:00

---
+++ extensions/extensions.py
@@ -0,0 +1,7 @@
+answer = input("File name:").strip().split(".")
+
+dict = {'gif':'image/gif', 'jpg':'image/jpeg', 'jpeg':'image/jpeg', 'png':'image/png', 'pdf':'application/pdf', 'txt':'text/plain', 'zip':'application/zip'}
+
+match type:
+    case .gif:
+        image/gif
