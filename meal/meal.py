c1568cacd3bab6fd61932cf89fa3e57fd5877daf
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:24:39 +0000

    Fri, Dec 1, 2023, 11:24 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,14 @@
 def main():
-     answer = int(input("What time is it?").strip().split(":"))
-     match(answer):
-          case x if 800 >= x >= 700: print("breakfast time")
-          case x if 1300 >= x >= 1200: print("lunch time")
-          case x if 1900 >= x >= 1800: print("dinner time")
+     hour, minute = input("What time is it?").strip().split(":")
+     total = convert(hour, minute)
+     match(total):
+          case x if 8*60 >= x >= 7*60: print("breakfast time")
+          case x if 13*60 >= x >= 12*60: print("lunch time")
+          case x if 19*60 >= x >= 18*60: print("dinner time")
 
+
+def convert(hour, minute):
+     return int(hour)*60 + int(minute)
 
 
 
