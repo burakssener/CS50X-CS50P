620cd163be7f4ebb75cdcf0e40c4136e9b0db17d
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 08:46:14 +0000

    Fri, Dec 1, 2023, 11:46 AM +03:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,17 +1,1 @@
-def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
-
-
-def dollars_to_float(d):
-    return float(d)
-
-
-
-def percent_to_float(p):
-    return float(p)
-
-
-main()
+print(format(123.4567, "^-09.3f"))
