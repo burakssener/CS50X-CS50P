93a98bf1cd380305d8723c9eaa3664866879b3f4
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:09:40 +0000

    Fri, Dec 1, 2023, 11:09 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,4 +1,5 @@
 def main():
+     answer = input("")
      match(time):
           case in range(7*60,8*60)
      between 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00.
