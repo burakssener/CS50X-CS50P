535d4794855efbf8f1285f7251220d6d8e5e0e9f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:59:55 +0000

    Fri, Dec 1, 2023, 11:59 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,8 @@
 price = 50
 while price > 0:
     print(f"Amount Due: {price}")
-    price -= int(input("Insert Coin:"))
+    if int(input("Insert Coin:")) in [5, 10, 25]:
+        price -= int(input("Insert Coin:"))
 
 print(f"Change Owed: {-1 *price}")
 
