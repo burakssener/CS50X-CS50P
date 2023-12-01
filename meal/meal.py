41e118c32c8ecb0031748b20cd74154123dd2e3a
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:15:05 +0000

    Fri, Dec 1, 2023, 11:15 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,8 +1,9 @@
 def main():
      answer = input("What time is it?").strip().split("#")
      match(answer):
-          case answer[0]
-     between 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00.
+          case 800 >= answer >= 700: print("breakfast time")
+          case 1300 >= answer >= 1200: print("lunch time")
+          case 1900 >= answer >= 1800: print("dinner time")
 
 
 def convert(time):
