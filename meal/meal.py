7404f24243c2c57c1a87da2abe5b2d01a48a9f44
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:41:02 +0000

    Fri, Dec 1, 2023, 11:41 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,14 +1,15 @@
 def main():
-     hour, minute = input("What time is it?").strip().split(":")
-     total = convert(hour, minute)
+     time= input("What time is it?")
+     total = convert(time)
      match(total):
-          case x if 8*60 >= x >= 7*60: print("breakfast time")
-          case x if 13*60 >= x >= 12*60: print("lunch time")
-          case x if 19*60 >= x >= 18*60: print("dinner time")
+          case x if 8 >= x >= 7: print("breakfast time")
+          case x if 13 >= x >= 12: print("lunch time")
+          case x if 19 >= x >= 18: print("dinner time")
 
 
-def convert(hour, minute):
-     return int(hour)*60 + int(minute)
+def convert(time):
+     hour, minute = time.strip().split(":")
+     return int(hour) + int(minute) / 60 * 100
 
 
 
