a9958f82d86db0e45b826b8d44b2c8df6e61ca84
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 21:11:39 +0000

    Sat, Dec 2, 2023, 12:11 AM +03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,10 +1,12 @@
-answer = input("Input: ").strip()
-print("Output: ", end="")
-for letter in answer:
-    if letter.upper() in ['A', 'E', 'I', 'O', 'U']:
-        continue
-    print(letter, end="")
+def main():
+    answer = input("Input: ").strip()
+    print("Output: ", end="")
+    for letter in answer:
+        if letter.upper() in ['A', 'E', 'I', 'O', 'U']:
+            continue
+        print(letter, end="")
 
-if __main__
+if __name__ == "__main__":
+    main()
 
 
