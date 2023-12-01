ce362eb48473315b8df3405cda484523f3dec64e
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:05:56 +0000

    Fri, Dec 1, 2023, 11:05 PM +03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,7 +1,11 @@
-answer = input("Expression:").strip()
-
-operations = ['/', '+', '-']
-
+answer = input("Expression:").strip().split()
+answer[0] = float(answer[0])
+answer[2] = float(answer[2])
+match answer[1]:
+    case '/': print(answer[0] / answer[2])
+    case '+': print(answer[0] + answer[2])
+    case '-': print(answer[0] - answer[2])
+    case '*': print(answer[0] * answer[2])
 
 
 
