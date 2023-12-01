e496a080c6de163d98fad90e89fa91bfaa6bc2a6
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 09:06:31 +0000

    Fri, Dec 1, 2023, 12:06 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,8 +1,8 @@
-answer = input("File name:").strip().split(".")
+answer = input("File name:").strip().lower().split(".")
 
 dict = {'gif':'image/gif', 'jpg':'image/jpeg', 'jpeg':'image/jpeg', 'png':'image/png', 'pdf':'application/pdf', 'txt':'text/plain', 'zip':'application/zip'}
 
-if answer[1] in dict.keys():
-    print(dict[answer[1]])
+if answer[-1] in dict.keys():
+    print(dict[answer[-1]])
 else:
     print("application/octet-stream")
