563db1f74245174a0e1d19885ba6b49e8734d719
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 21:08:57 +0000

    Sat, Dec 2, 2023, 12:08 AM +03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,6 +3,6 @@ print("Output: ", end="")
 for letter in answer:
     if letter.upper() in ['A', 'E', 'I', 'O', 'U']:
         continue
-    else:
-        print(letter, end="")
+    print(letter, end="")
+
 
