cb9d4db2bd4bd9cd072b54050f9ed0ef924ca6d1
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-01 20:50:11 +0000

    Fri, Dec 1, 2023, 11:50 PM +03:00

---
+++ camel/camel.py
@@ -0,0 +1,9 @@
+answer = input("camelCase: ").strip()
+print("snake_case: ", end="")
+for letter in answer:
+    if not letter.isupper():
+        print(letter, end="")
+    else:
+        print("_",letter.lower(), end="")
+print()
+
