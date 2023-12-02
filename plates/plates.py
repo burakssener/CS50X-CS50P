1a39ef19137d43bdb88bdf6184c1fefbb7bc30b4
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 07:15:08 +0000

    Sat, Dec 2, 2023, 10:15 AM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -2,6 +2,7 @@ answer = input("Plate: ").strip()
 
 if not (6 >= len(answer) >=2):
     print("Invalid")
-elif not (for letter in answer[0:2] if letter.isalpha()):
-    print("Invalid")
-x
+for letter in answer:
+    if not (letter.isdigit() or letter.isalpha()):
+        print("Invalid")
+
