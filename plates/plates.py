b144d9f8b19dcb76054bdb974eeac8cb22ba4671
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 07:31:00 +0000

    Sat, Dec 2, 2023, 10:31 AM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -5,26 +5,22 @@ if not (6 >= len(answer) >=2):
 else:
     for letter in answer[0:2]:
         if not letter.isalpha():
-            status = 0
-    if status == 0:
-        print("Invalid")
-    else:
+            print("Invalid")
+            break
+    for letter in answer:
+        if not (letter.isdigit() or letter.isalpha()):
+            print("Invalid")
+            break
+    if answer[-1].isalpha():
         for letter in answer:
-            if not (letter.isdigit() or letter.isalpha()):
-                status = 0
+                if letter.isdigit():
+                    status = 0
         if status == 0:
             print("Invalid")
         else:
-            if answer[-1].isalpha():
-                for letter in answer:
-                        if letter.isdigit():
-                            status = 0
-                if status == 0:
-                    print("Invalid")
-                else:
-                    print("Valid")
-            else:
-                 print("Valid")
+            print("Valid")
+    else:
+            print("Valid")
 
 
 
