6839161a4ac15593e5688fc1a463e29feb5bd44d
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 09:12:13 +0000

    Sat, Dec 2, 2023, 12:12 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,7 @@
 while True:
     try:
-        expression = input("Fraction: ").strip()
-        result = int(expression[0]) / int(expression[2]) * 100
+        x ,y = input("Fraction: ").strip().split("/")
+        result = int(x) / int(y) * 100
         if result >= 99:
             print("F")
         elif result <= 1:
