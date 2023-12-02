6e325282fbcdbf83efdf68fb29ff3a17c04b8db9
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 09:08:23 +0000

    Sat, Dec 2, 2023, 12:08 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,10 +1,12 @@
 while True:
     try:
         x , slash, y = input("Fraction: ").strip().split()
-        result = x / y * 100
+        result = int(x) / int(y) * 100
         if result >= 99:
             print("F")
         elif result <= 1:
             print("E")
         else:
             print("%", result, sep="")
+    except ValueError and ZeroDivisionError:
+        pass
