8bcf9b6acd0fc205fbba389256ccb8f7663c0d15
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 13:23:47 +0000

    Sat, Dec 2, 2023, 4:23 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,5 +13,13 @@ monhths = [
     "December"
 ]
 while True:
-    raw_date = input("Date: ")
+    raw_date = input("Date: ").strip()
+    try:
+        month, day, year = raw_date.split("/")
+    except ValueError:
+        month, day, year = raw_date.split(" ")
+        day = day.split(",")
+
+    print(f"{year:0004}-{month:02}-{day:02}")
+
 
