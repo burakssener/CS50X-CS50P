9c98b795cb4d581d447b99d684b4f72b275a0165
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 13:07:45 +0000

    Sat, Dec 2, 2023, 4:07 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -7,7 +7,7 @@ while True:
         else:
             items[new_item] = 1
     except EOFError:
-        for item in items.keys().sorted():
+        for item in items.keys().sort():
             print(items[item], item)
         break
 
