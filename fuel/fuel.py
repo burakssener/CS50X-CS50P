b9522700b5eb632ef3a5938d0f133681bc378396
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 09:18:23 +0000

    Sat, Dec 2, 2023, 12:18 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,9 @@
 while True:
     try:
         x ,y = input("Fraction: ").strip().split("/")
-        if int(x) in [1,3] and int(y) in [1]
+        if not (int(x) in [1,3] and int(y) in [2,4]):
+            pass
         result = int(x) / int(y) * 100
-
         if result >= 99:
             print("F")
         elif result <= 1:
