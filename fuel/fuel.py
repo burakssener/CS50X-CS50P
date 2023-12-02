c1aef421e1aa9fe6cbf74b043d733149b8d23b8d
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 09:07:23 +0000

    Sat, Dec 2, 2023, 12:07 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,5 +1,10 @@
 while True:
-    x , slash, y = input("Fraction: ").strip().split()
-    result = x / y * 100
-    if result >= 99:
-    e
+    try:
+        x , slash, y = input("Fraction: ").strip().split()
+        result = x / y * 100
+        if result >= 99:
+            print("F")
+        elif result <= 1:
+            print("E")
+        else:
+            print("%", result, sep="")
