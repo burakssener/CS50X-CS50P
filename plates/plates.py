2ebb4a4605d1af5ad4164c10a91b641164fee0d4
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-02 07:19:39 +0000

    Sat, Dec 2, 2023, 10:19 AM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,10 +1,21 @@
 answer = input("Plate: ").strip()
-status = 0
+status = 1
 if not (6 >= len(answer) >=2):
     print("Invalid")
 else:
-    for letter in answer:
-        if not (letter.isdigit() or letter.isalpha()):
+    for letter in answer[0:2]:
+        if not letter.isalpha():
+            status = 0
+    if status == 0:
+        print("Invalid")
+    else:
+        for letter in answer:
+            if not (letter.isdigit() or letter.isalpha()):
+                status = 0
+        if status == 0:
             print("Invalid")
+        else:
+
+
 
 
