1533188603074b98e45f51863bac703657cbeb51
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 09:03:50 +0000

    Mon, Dec 4, 2023, 12:03 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,6 +3,7 @@ from sys import argv, exit
 from random import choice
 
 if not len(argv) in [1, 3]:
+    print("Error")
     exit()
 figlet = Figlet()
 text = input("Input: ").strip().lower()
@@ -15,7 +16,9 @@ elif len(argv) == 3 and argv[1] in ["-f", "--font"]:
     if argv[2] in figlet.getFonts():
         figlet.setFont(font=argv[2])
         print(figlet.renderText(text))
-    else
+    else:
+        print("Error")
+        exit()
 
 
 
