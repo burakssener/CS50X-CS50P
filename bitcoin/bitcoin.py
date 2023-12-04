020032f157c1b1617105eeb3acc60b794df9908c
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 11:23:39 +0000

    Mon, Dec 4, 2023, 2:23 PM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,6 +2,6 @@ import requests
 
 try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    print(response)
+    print(response.json())
 except requests.RequestException:
     ...
