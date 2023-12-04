937127e8a26e1df8e980c6826bb0e75133d4ae9d
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 10:46:52 +0000

    Mon, Dec 4, 2023, 1:46 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,17 +2,20 @@ import random
 
 
 def main():
-    ...
+    input(f"{x} + {y} = ", end="")
+
 
 
 def get_level():
-    ...
+    while True:
+        input("Level: ")
+        if input in [1, 2, 3]:
+            return input
 
 
 def generate_integer(level):
     x = random.randrange(level*10)
-    y = random.randrange(level*10)
-    return f"{x} + {y} = "
+
 
 
 if __name__ == "__main__":
