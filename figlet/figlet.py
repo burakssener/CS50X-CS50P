c4ce088b14a72823483e1bd2eac22b6466e74634
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 08:55:08 +0000

    Mon, Dec 4, 2023, 11:55 AM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,17 @@
 from pyfiglet import Figlet
-import sys
+from sys import argv
+from random import choice
 
-figlet = Figlet()
-for fonts in figlet.getFonts():
-    print(fonts)
+if len(argv) == 1:
+    figlet = Figlet()
+    text = input("Input: ").strip().lower()
+    f = choice(figlet.getFonts())
+    figlet.setFont(font=f)
+    print(figlet.renderText(text))
+
+elif len(argv) == 3:
+    print()
+
+else:
+    print()
 
