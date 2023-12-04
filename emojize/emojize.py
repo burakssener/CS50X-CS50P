3644453c7edd809d1a2e1425dd52759911189acd
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 08:42:23 +0000

    Mon, Dec 4, 2023, 11:42 AM +03:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,10 +2,7 @@ import emoji
 
 answer = input("Input: ").strip()
 
-list = answer.split()
-for letter in answer:
-    if letter != ':':
-        print(letter, end="")
-    else:
+print(emoji.emojize(answer ,language='alias'))
+
 #answer = input("Input: ").strip().lower().replace("_", "")
 #print(emoji.emojize(answer ,language='alias'))
