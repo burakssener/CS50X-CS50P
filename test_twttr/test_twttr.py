a43bad53d28d695fc2d9cd44634d8208b41995f7
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 13:21:01 +0000

    Mon, Dec 4, 2023, 4:21 PM +03:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,9 @@
 from twttr import shorten
 
-def test_shorten():
-    assert shorten("12321ada") == ("12321d")
 
-def test_shorten():
+
+def test1_shorten():
     assert shorten("12321ede") == ("12321d")
+
+def test2_shorten():
+    assert shorten("12321ada") == ("12321d")
