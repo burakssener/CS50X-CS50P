374a7efbc9001434752b75596e1ab5c9686ee943
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 12:56:18 +0000

    Mon, Dec 4, 2023, 3:56 PM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,14 +4,16 @@ from sys import argv, exit
 if len(argv) != 2:
     print("Missing command-line argument")
     exit(1)
-if not argv[1].isdigit():
+try:
+    number = float(argv[1])
+except:
     print("Command-line argument is not a number")
     exit(1)
 
 try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     amount = float((response.json()["bpi"]["USD"]["rate"]).replace(",",""))
-    amount *= float(argv[1])
+    amount *= number
     print(f"${amount:,.4f}")
 except requests.RequestException:
     print("request error occured")
