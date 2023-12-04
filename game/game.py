ef2b64b3c9c24aa0822f01043e31326f29227ae2
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-04 09:58:10 +0000

    Mon, Dec 4, 2023, 12:58 PM +03:00

--- game/game.py
+++ game/game.py
@@ -1,3 +1,34 @@
 import random
 
-print(random.randrange(10))
+def main():
+    level = take_digit("Level: ")
+    number = random.randrange(level)
+    while True:
+        answer = take_digit("Guess: ")
+        if (answer == number):
+            print("Just right!")
+            break
+        elif (answer <= number):
+            print("Too small!")
+        else:
+            print("Too large!")
+
+
+
+def take_digit(prompt):
+    digit = input(prompt)
+    while True:
+        if not digit.isdigit():
+            digit = input(prompt)
+        else:
+            if int(digit) > 0:
+                return int(digit)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
