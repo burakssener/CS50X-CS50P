ba4daf57f5cb3d74743e95183ba3a99befd981d6
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-05 05:13:29 +0000

    Tue, Dec 5, 2023, 8:13 AM +03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,20 @@
 from plates import is_valid
 
-t
+def test_twoletter():
+    assert is_valid("as345") == 1
+    assert is_valid("AS345") == 1
+    assert is_valid("A4345") == 0
+    assert is_valid("a4345") == 0
+def test_length():
+    assert is_valid("ASA3333") == 0
+    assert is_valid("A") == 0
+    assert is_valid("ASA333") == 1
+def test_numbers():
+    assert is_valid("C50C") == 0
+    assert is_valid("85SDS") == 0
+    assert is_valid("ASDA8") == 1
+def punctuation():
+    assert is_valid("AS,DA8") == 0
+    assert is_valid(",,SDS") == 0
+    assert is_valid("ASSDS") == 1
+
