6cbc7eb99735c30b872aba17fea774764b341118
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-06 12:33:46 +0000

    Wed, Dec 6, 2023, 3:33 PM +03:00

---
+++ pizza/pizza.py
@@ -0,0 +1,23 @@
+from sys import argv
+
+if len(argv) > 2:
+    exit("Too many command-line arguments ")
+
+elif len(argv) < 2:
+    exit("Too few command-line arguments ")
+
+elif not argv[1].endswith(".csv"):
+    exit("Not a CSV file")
+
+else:
+    try:
+        code_lines = 0
+        with open(argv[1], "r") as file:
+            for line in file:
+                line = line.rstrip()
+                if line and (not line.startswith("#")):
+                    code_lines += 1
+        print(code_lines)
+
+    except FileNotFoundError:
+        exit("File does not exist")
