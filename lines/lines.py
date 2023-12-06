daf2622b46204c76244922c61c3e934c760c710f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-06 12:11:26 +0000

    Wed, Dec 6, 2023, 3:11 PM +03:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,18 +1,18 @@
 from sys import argv
 
-if len(argv) > 3:
+if len(argv) > 2:
     exit("Too many command-line arguments ")
 
-elif len(argv) < 3:
+elif len(argv) < 2:
     exit("Too few command-line arguments ")
 
-elif not argv[2].endswith(.py):
+elif not argv[1].endswith(".py"):
     exit("Not a Python file")
 
 else:
     try:
         code_lines = 0
-        with open(argv[2], "r") as file:
+        with open(argv[1], "r") as file:
             for line in file:
                 line = line.rstrip()
                 if line and not line.startswith("#"):
