39b3fa7d96c664a68f9d4b6129d8206b0d62b2b3
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-06 12:50:13 +0000

    Wed, Dec 6, 2023, 3:50 PM +03:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,25 @@
+from sys import argv
+import csv
+from tabulate import tabulate
+
+if len(argv) > 2:
+    exit("Too many command-line arguments ")
+
+elif len(argv) < 2:
+    exit("Too few command-line arguments ")
+
+elif not argv[1].endswith(".csv"):
+    exit("Not a CSV file")
+
+else:
+    try:
+        data = []
+        with open(argv[1], "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
+        print(data)
+        print(tabulate(data, headers="keys", tablefmt="grid"))
+
+    except FileNotFoundError:
+        exit("File does not exist")
