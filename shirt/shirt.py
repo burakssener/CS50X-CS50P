505e96edb44ebf812de86918fe074d8cfc5d589f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-07 04:54:34 +0000

    Thu, Dec 7, 2023, 7:54 AM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -8,21 +8,8 @@ elif len(argv) < 3:
 
 elif not (argv[1][-4:] and argv[2][-4:] in [".jpg", ".png", "jpeg"]):
     exit("Invalid input")
-elif not argv[1][-3:] != argv[2][-3:]:
+elif argv[1][-3:] != argv[2][-3:]:
     exit("Input and output have different extensions")
 else:
-    print("OKKAYY")
+    print()
 
-"""else:
-    try:
-        data = []
-        with open(argv[1], "r") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                data.append(row)
-
-        print(tabulate(data, headers="keys", tablefmt="grid"))
-
-    except FileNotFoundError:
-        exit("File does not exist")
-"""
