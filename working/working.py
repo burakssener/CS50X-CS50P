678a1cc8989afc4c0ff84d7bce6305fdfad8dc59
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-10 12:22:16 +0000

    Sun, Dec 10, 2023, 3:22 PM +03:00

--- working/working.py
+++ working/working.py
@@ -8,21 +8,21 @@ def main():
 
 def convert(s):
     if match := re.search("^([0-9][0-2]?(?::[0-5][0-9])?) ([ap]m) to ([0-9][0-2]?(?::[0-5][0-9])?) ([ap]m)$", s, re.IGNORECASE):
-        if match.group(2).endswith("pm"):
-            try:
-                hour, minute = match.group(1).split(":")
-                hour, minute = int(hour) + 12, int(minute)
-            except ValueError:
-        if match.group(4).endswith("pm"):
-            try:
-                hour, minute = match.group(3).split(":")
-                hour, minute = int(hour) + 12, int(minute)
-            except ValueError:
-                int(match.group(3)) + 12
-
-
+        return f"{clock_converter(match.group(1), match.group(2))} to {clock_converter(match.group(3), match.group(4))}"
     else:
         return match
+
+def clock_converter(time, specifier):
+    #input types 9:30 or 9 and the function return 9 and 30 or 9 and 00 as a string and add 12 if the second parameter is 1
+    if ':' in time:
+        hour, minute = time.split(":")
+    else:
+        hour, minute = time, "00"
+
+    if specifier == "pm":
+         hour = int(hour) + 12
+
+    return f"{hour}:{minute}"
 
 
 
