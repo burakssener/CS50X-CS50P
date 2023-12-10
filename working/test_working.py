50f0d2c37c67444a14219703b7abbda74106a77b
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-10 13:06:37 +0000

    Sun, Dec 10, 2023, 4:06 PM +03:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,20 +1,20 @@
 from working import convert
-
+import pytest
 def test_convertionforpm():
-    assert convert("5 AM to 9 PM") = ("05.00 to 21.00")
-    assert convert("8 AM to 8 PM") = ("8.00 to 16.00")
+    assert convert("5 AM to 9 PM") == ("05:00 to 21:00")
+    assert convert("8 AM to 8 PM") == ("08:00 to 20:00")
 
 def test_convertionforPmAmWithCorners():
-    assert convert("12 AM to 9 PM") = ("00.00 to 21.00")
-    assert convert("5 AM to 12 PM") = ("05.00 to 12.00")
+    assert convert("12 AM to 9 PM") == ("00:00 to 21:00")
+    assert convert("5 AM to 12 PM") == ("05:00 to 12:00")
 
 def test_convertionforValueError():
     with pytest.raises(ValueError) as error:
-        convert("5 AM to 9.60 PM")
+        convert("5 AM to 9:60 PM")
     assert error.type is ValueError
-     with pytest.raises(ValueError) as error:
+    with pytest.raises(ValueError) as error:
         convert("13 AM to 9 PM")
     assert error.type is ValueError
-    
+
 
 
