935e51f64519ffba12b991501272d310debbefbf
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-10 05:39:50 +0000

    Sun, Dec 10, 2023, 8:39 AM +03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -8,13 +8,14 @@ def main():
 
 def validate(ip):
     ip = ip.strip()
-    match = re.search(r"(^[0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$")
+    match = re.search(r"^(0-9+)\.(0-9+)\.(0-9+)\.(0-9+)$")
     try:
-        match.groups(1), match.groups(2), match.groups(3) = 
+        print(match.groups())
+    except:
+        raise ValueError
 
 
 
-...
 
 
 if __name__ == "__main__":
