b2043fd42782e3452edac90801aa37c4ec49aa4d
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-10 12:24:51 +0000

    Sun, Dec 10, 2023, 3:24 PM +03:00

--- working/working.py
+++ working/working.py
@@ -8,9 +8,12 @@ def main():
 
 def convert(s):
     if match := re.search("^([0-9][0-2]?(?::[0-5][0-9])?) ([ap]m) to ([0-9][0-2]?(?::[0-5][0-9])?) ([ap]m)$", s, re.IGNORECASE):
-        return f"{clock_converter(match.group(1), match.group(2))} to {clock_converter(match.group(3), match.group(4))}"
+        try:
+            return f"{clock_converter(match.group(1), match.group(2))} to {clock_converter(match.group(3), match.group(4))}"
+        except ValueError:
+            raise ValueError
     else:
-        return match
+        return None
 
 def clock_converter(time, specifier):
     #input types 9:30 or 9 and the function return 9 and 30 or 9 and 00 as a string and add 12 if the second parameter is 1
@@ -19,7 +22,7 @@ def clock_converter(time, specifier):
     else:
         hour, minute = time, "00"
 
-    if specifier == "pm":
+    if specifier.lower() == "pm":
          hour = int(hour) + 12
 
     return f"{hour}:{minute}"
