fce8a95f88cfe31a224cba417dfe5c2390564a4f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-17 16:44:46 +0000

    Sun, Dec 17, 2023, 7:44 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -7,8 +7,8 @@ def main():
     birth = getter()
 
     minute = round((date.today() - birth).total_seconds() / 60)
-    words = p.number_to_words(minute)
-    print(words + " minutes")
+    words = p.number_to_words(minute, andword="")
+    print(words.capitalize() + " minutes")
 
 def getter():
     while True:
