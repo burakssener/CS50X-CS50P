09ef3bfd9d17486fd989bf57b5928426f28a4404
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-17 18:29:24 +0000

    Sun, Dec 17, 2023, 9:29 PM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,12 +1,13 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        capacity(self)
+        size(self)
 
     def __str__(self):
         ...
 
     def deposit(self, n):
-        ...
+        ...3
 
     def withdraw(self, n):
         ...
