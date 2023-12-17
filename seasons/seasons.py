df762dbce698b8d8d2b9e103bebd438d0b1909c7
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-17 12:14:06 +0000

    Sun, Dec 17, 2023, 3:14 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -6,6 +6,14 @@ def main():
 
 
 def getter():
+    while True:
+        try:
+            birth = int(input("Date of Birth: ").strip().replace(" ", ""))
+            break
+        except ValueError:
+            pass
+
+    birth = birth.date.fromisoformat('20191204')
     print(date.today())
 
 if __name__ == "__main__":
