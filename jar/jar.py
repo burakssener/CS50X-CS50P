8e543eb16ddba85e0bed4cb4bf56ed5b610b2390
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 05:52:37 +0000

    Tue, Dec 19, 2023, 8:52 AM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -2,6 +2,15 @@ class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
 
+    def __str__(self):
+        return f"{self.capacity}"
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
     @property
     def capacity(self):
         return self._capacity
@@ -9,9 +18,11 @@ class Jar:
 
     @capacity.setter
     def capacity(self, capacity):
-        return self._capacity
+        self._capacity = capacity
 
 
 cookie = Jar(12)
+
+print(cookie)
 
 
