9830ceebafea2a0fd623dcbe6cf1f9406c4ab5ef
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 06:33:25 +0000

    Tue, Dec 19, 2023, 9:33 AM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,5 +2,8 @@ from seasons import getter
 
 def test_textCheck():
     assert getter("  2022-12-12 ") == "2022-12-12"
-    assert getter("  2022--12-12 ") == "2022-12-12"
+    with pytest.raises(SystemExit) as error_code:
+        function_that_may_exit()
+
+    assert error_code.value.code == 1
 
