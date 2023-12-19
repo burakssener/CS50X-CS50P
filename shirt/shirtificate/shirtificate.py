2d3c87599ea97dcf23ada185a625de2f7aa0d738
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 10:53:19 +0000

    Tue, Dec 19, 2023, 1:53 PM +03:00

--- shirt/shirtificate/shirtificate.py
+++ shirt/shirtificate/shirtificate.py
@@ -1,26 +1,10 @@
 from fpdf import FPDF
 class PDF():
     def __init__(self, name):
-        self._pdf = FPDF()
-        self._pdf.add_page()
+        self.pdf = FPDF(orientation="portrait", format="A4")
         self._pdf.set_font("helvetica", "B", 50)
-    def header(self):
-        # Rendering logo:
-        self.image("./shirtificate.png", 10, 8, 33)
-        # Setting font: helvetica bold 15
-        self.set_font("helvetica", "B", 15)
-        # Moving cursor to the right:
-        self.cell(80)
-        # Printing title:
-        self.cell(30, 10, "Title", border=1, align="C")
-        # Performing a line break:
-        self.ln(20)
+        self._pdf.add_page()
+        pdf.cell(text=""CS50 Shirtificate"", markdown=True)
+
 
 pdf = PDF()
-
-#pdf.set_font('helvetica')
-#pdf.cell(text="hello world")
-pdf.set_title("CS50 Shirtificate")
-pdf.cell(30, 10, "Title", border=1, align="C")
-pdf.image("./shirtificate.png")
-pdf.output("hello_world.pdf")
