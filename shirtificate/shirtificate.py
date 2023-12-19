6bad213ea374147956791f051c06d721c2e3e3ec
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 11:38:14 +0000

    Tue, Dec 19, 2023, 2:38 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,13 +4,13 @@ class PDF():
         self._pdf = FPDF(orientation="portrait", format="A4")
         self._pdf.add_page()
         self._pdf.set_font("helvetica", "B", 50)
-        self._pdf.cell(self, , ln='DEPRECATED', fill=False, link='', center=True, markdown=False, text="CS50 Shirtificate")
+        self._pdf.cell(text="CS50 Shirtificate", center = True, markdown=True)
 
     def save(self, name):
         self._pdf.output(name)
 
     def image(self, path):
-        self._pdf.image(path)
+        self._pdf.image(path, w=self._pdf.epw)
 
 
 
