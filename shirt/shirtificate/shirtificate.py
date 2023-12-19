0e100b8df1c9da514d183845d86bb6725e691795
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 10:30:12 +0000

    Tue, Dec 19, 2023, 1:30 PM +03:00

--- shirt/shirtificate/shirtificate.py
+++ shirt/shirtificate/shirtificate.py
@@ -4,4 +4,7 @@ pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', size=12)
 pdf.cell(text="hello world")
+pdf.image(
+    "../docs/fpdf2-logo.png", 10, 10, 50, 0, "", "https://py-pdf.github.io/fpdf2/"
+)
 pdf.output("hello_world.pdf")
