6b3ef8af734036fd5326c9bfd54230eb21511d7c
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 10:57:02 +0000

    Tue, Dec 19, 2023, 1:57 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,9 +2,14 @@ from fpdf import FPDF
 class PDF():
     def __init__(self, name):
         self._pdf = FPDF(orientation="portrait", format="A4")
-        self._pdf.set_font("helvetica", "B", 50)
         self._pdf.add_page()
+        self._pdf.set_font("helvetica", "B", 50)
         self._pdf.cell(text="CS50 Shirtificate", markdown=True)
+
+    def save(self, name):
+        FPDF
+
 
 
 pdf = PDF("cs50")
+pdf.save("Shirtificate.pdf")
