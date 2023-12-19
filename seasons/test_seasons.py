e7a19bcd3f70e6700da60580cdf67e5e9c58db61
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 06:31:57 +0000

    Tue, Dec 19, 2023, 9:31 AM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,5 +1,6 @@
 from seasons import getter
 
 def test_textCheck():
-    assert getter("  2022-12-12 ") = "2022-12-12"
+    assert getter("  2022-12-12 ") == "2022-12-12"
+    assert getter("  2022--12-12 ") == "2022-12-12"
 
