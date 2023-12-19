27f6ec2493496973a494fc3ff4f19cefc07cbc5c
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-19 10:42:58 +0000

    Tue, Dec 19, 2023, 1:42 PM +03:00

--- shirt/shirtificate/shirtificate.py
+++ shirt/shirtificate/shirtificate.py
@@ -2,7 +2,7 @@ from fpdf import FPDF
 class PDF(FPDF):
     def header(self):
         # Rendering logo:
-        self.image("../docs/fpdf2-logo.png", 10, 8, 33)
+        self.image("./shirtificate.png", 10, 8, 33)
         # Setting font: helvetica bold 15
         self.set_font("helvetica", "B", 15)
         # Moving cursor to the right:
